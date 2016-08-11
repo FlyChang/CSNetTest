0ad3fd142cba7fbed27fc2991e3f25a15d508ce4
LiuChang <986957270@qq.com>
2016-08-11 13:14:44 +0800

    commit

---
+++ CSTestDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CSTestDemo
+//
+//  Created by 刘畅 on 16/8/11.
+//  Copyright © 2016年 LiuChang. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let json = JSON([:])
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
